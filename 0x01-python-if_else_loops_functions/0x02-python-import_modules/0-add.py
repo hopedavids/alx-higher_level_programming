1ae19ec82553770b8bbfdb5f7cdb6a41eccc4bc3
Hope_Davids <hledvaids@gmail.com>
2023-01-09 15:06:15 +0000

    add file

--- 0x01-python-if_else_loops_functions/0x02-python-import_modules/0-add.py
+++ 0x01-python-if_else_loops_functions/0x02-python-import_modules/0-add.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-import add_0
-add_val = add(a,b)
-print("{} + {} = {}".format(a,b,add_val))
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    add_val = add(a, b)
+    print("{} + {} = {}".format(a,b,add_val))
