0ea7d6b356dcfbcde88b846cd24e436d95275a57
Hope_Davids <hopedavids@gmail.com>
2023-04-13 11:09:27 +0000

    javascript file that write to stdout

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -2,5 +2,6 @@
 
 // This is a simple javascript file to write to stdout
 const myVar = "JavaScript is amazing";
-
+// myVar is the name of the variable to write to stdout
 console.log(myVar);
+// console.log(myVar) write to stdout
