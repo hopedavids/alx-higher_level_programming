9cca6776a74d2c15e9547affaa7d24c63ede9240
Hope_Davids <hopedavids@gmail.com>
2023-04-13 11:16:44 +0000

    write to stdout mult-language

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// The first line: “C is fun”
+const first = "C is fun";
+console.log(first);
+// The second line: “Python is cool”
+const second = "Python is cool";
+console.log(second);
+// The third line: “JavaScript is amazing”
+const third = "JavaScript is amazing";
+console.log(third);
+// You must use console.log(...) to print all output
+// You are not allowed to use var
