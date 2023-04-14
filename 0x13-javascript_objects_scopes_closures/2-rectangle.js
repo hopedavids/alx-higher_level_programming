0d0393f57ff0ec0fad2a22b689104d41a3a85e25
Hope_Davids <hopedavids@gmail.com>
2023-04-14 12:45:25 +0000

    validating parameter of the rectangle

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -10,8 +10,9 @@
 class Rectangle {
 
     constructor(w, h) {
-        if (w <= 0 || h <= 0) {
+        if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
 // If w or h is equal to 0 or not a positive integer, create an empty object]
+            return;
         }
         else {
             this.w = w;
