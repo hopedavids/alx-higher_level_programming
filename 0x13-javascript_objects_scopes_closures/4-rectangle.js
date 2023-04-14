95267a48168719d0e264643e441f6ff83829b4e8
Hope_Davids <hopedavids@gmail.com>
2023-04-14 13:19:47 +0000

    rotating the rectangle

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -11,31 +11,23 @@
 // Create an instance method called rotate() that exchanges the width and the height of the rectangle
 // Create an instance method called double() that multiples the width and the height of the rectangle by 2
 
-class Rectangle {
-    constructor(w, h) {
-      if (w <= 0 || h <= 0) {
-        return {};
-      }
+module.exports = {
+  Rectangle: function (w, h) {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
-  
-    print() {
-      const row = "X".repeat(this.width);
+    this.print = function () {
       for (let i = 0; i < this.height; i++) {
-        console.log(row);
+        console.log(('X').repeat(this.width));
       }
-    }
-  
-    rotate() {
-      const temp = this.width;
-      this.width = this.height;
-      this.height = temp;
-    }
-  
-    double() {
+    };
+    this.rotate = function () {
+      [this.width, this.height] = [this.height, this.width];
+    };
+    this.double = function () {
       this.width *= 2;
       this.height *= 2;
-    }
-}
-  
+    };
+  }
+};
