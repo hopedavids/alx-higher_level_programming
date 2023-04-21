ecf8131c856ec3be41010f51fbd27210525bf1b3
Hope_Davids <hopedavids@gmail.com>
2023-04-21 10:46:03 +0000

    sqlalchemy database connection

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+'''
+python file that contains the 
+class definition of a State and an instance Base 
+'''
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import sys
+
+Base = declarative_base()
+
+class State(Base):
+    ''' Class containing the state attributes and methods
+        Also inherits from the declarative_base class
+    '''
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+
+        
+    def __repr__(self):
+        return (f'{self.name} {self.id})')
+    
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+
