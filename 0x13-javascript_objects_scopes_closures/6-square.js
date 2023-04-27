01ed8bd80ada46037361bda4d6e605e9ea7075d7
Hope_Davids <hopedavids@gmail.com>
2023-04-27 06:39:09 +0000

    square_01

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,19 +1,10 @@
 #!/usr/bin/node
-
-const Rectangle = require('./5-rectangle');
-
-class Square extends Rectangle {
-  constructor(size) {
-    super(size, size);
-  }
-
-  charPrint(c) {
+module.exports = class Square extends require('./5-square.js') {
+  charPrint (c) {
     if (c === undefined) {
-      c = "X";
-    }
-    const row = c.repeat(this.width);
-    for (let i = 0; i < this.height; i++) {
-      console.log(row);
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
     }
   }
-}
+};
