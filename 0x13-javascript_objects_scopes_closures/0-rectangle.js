e459d767fe486008c58e7bf34b4d836cc35f4e9d
Hope_Davids <hopedavids@gmail.com>
2023-04-27 06:23:17 +0000

    modified class object

--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,11 +1,2 @@
 #!/usr/bin/node
-
-// Write an empty class Rectangle that defines a rectangle:
-class Rectangle {
-
-    constructor(width, height) {
-        this.width = width;
-        this.height = height;
-    }
-}
-// You must use the class notation for defining your class
+module.exports = class Rectangle {};
