8eaa4dec1fb5a65c6218247d25b77983ecad8e04
Hope_Davids <hopedavids@gmail.com>
2023-04-27 06:26:12 +0000

    declaring the class as a module

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,14 +1,9 @@
 #!/usr/bin/node
-
 // Write a class Rectangle that defines a rectangle:
-class Rectangle {
-
-    constructor(w, h){
-        this.width = w;
-        this.height = h;
-    }
-}
 // You must use the class notation for defining your class
-// The constructor must take 2 arguments w and h
-// Initialize the instance attribute width with the value of w
-// Initialize the instance attribute height with the value of h
+module.exports = class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
