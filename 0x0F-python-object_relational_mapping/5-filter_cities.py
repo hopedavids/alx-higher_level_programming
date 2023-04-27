ec467484241896ab75f5f70b6c28a80b1051794d
Hope_Davids <hopedavids@gmail.com>
2023-04-27 07:31:41 +0000

    display all cities from a given table

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,33 +1,19 @@
 #!/usr/bin/python3
-""" 
-    filter cities, write one that is safe from MySQL injections!
-"""
-import MySQLdb as mydb
+# Displays all cities of a given state from the
+# states table of the database hbtn_0e_4_usa.
+# Safe from SQL injections.
+# Usage: ./5-filter_cities.py <mysql username> \
+#                             <mysql password> \
+#                             <database name> \
+#                             <state name searched>
 import sys
+import MySQLdb
 
-#  mysql username, mysql password, database name and state name searched
-# the function that work on safe mysql filter
-def filter_cities():
-
-    # MySQL connection initialization
-    db = mydb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    # create the cursor object
-    cursor = mydb.cursor()
-    state = sys.argv[4]
-    #input the command to query the database
-    command = ("SELECT cities.id, cities.name, states.name FROM cities \
-         JOIN states ON cities.state_id = states.id \
-         WHERE states.name = %s \
-         ORDER BY cities.id ASC", (state))
-    #execute the command
-    cursor.execute(command)
-    # fetch all the rows
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
-    
-    cursor.close()
-    db.close()
-
-    if __name__ == "__main__":
-        filter_cities()
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `cities` as `c` \
+                INNER JOIN `states` as `s` \
+                   ON `c`.`state_id` = `s`.`id` \
+                ORDER BY `c`.`id`")
+    print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
