c90ab042baa4d3dd51238d99ab8f05c6007dbb3d
Hope_Davids <hopedavids@gmail.com>
2023-04-27 07:21:08 +0000

    listing all states in the database

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,27 +1,13 @@
 #!/usr/bin/python3
-""" This script lists all states from
-    the database hbtn_0e_0_usa
-"""
-import MySQLdb as mydb
+# Lists all states from the database hbtn_0e_0_usa.
+# Usage: ./0-select_states.py <mysql username> \
+#                             <mysql password> \
+#                             <database name>
 import sys
+import MySQLdb
 
-def hbtn_Connect():
-    db = mydb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-
-    cursor = mydb.cursor()
-
-    command = "SELECT * FROM states ORDER BY id ASC"
-    # excute the sql statement
-    cursor.execute(command)
-    # Fetch all the rows
-    rows = cursor.fetchall()
-    #print  each city in the state rows
-    for row in rows:
-        print(row)
-
-
-    cursor.close()
-    db.close()
-
-    if __name__ == "__main__":
-        hbtn_Connect()
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall()]
