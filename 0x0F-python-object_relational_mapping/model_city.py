913935d546d9db0a1999bf8e72d1aaaccd96e87f
Hope_Davids <hopedavids@gmail.com>
2023-04-27 07:33:15 +0000

    model city

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,31 +1,22 @@
 #!/usr/bin/python3
-"""
-Script that prints the State object with the name passed as argument
-from the database hbtn_0e_6_usa.
-"""
-import sys
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from model_state import Base
+# Defines a City model.
+# Inherits from SQLAlchemy Base and links to the MySQL table cities.
 
-"""
-City class:
-inherits from Base (imported from model_state)
-links to the MySQL table cities
-class attribute id that represents a column of an auto-generated, unique integer, 
-can’t be null and is a primary key
-class attribute name that represents a column of a string of 128 characters and 
-can’t be null
-class attribute state_id that represents a column of an integer, can’t be null and is 
-a foreign key to states.id
-You must use the module SQLAlchemy 
-"""
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
 
 class City(Base):
-    __tablename__ = 'city'
+    """Represents a city for a MySQL database.
 
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    Attributes:
+        id (str): The city's id.
+        name (sqlalchemy.Integer): The city's name.
+        state_id (sqlalchemy.String): The city's state id.
+    """
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('state_id'),nullable=False)
-
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
