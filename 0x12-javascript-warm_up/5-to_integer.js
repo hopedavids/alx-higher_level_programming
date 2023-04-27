788c9275fcf78146f271724eff48d3c9b6a8a0db
Hope_Davids <hopedavids@gmail.com>
2023-04-27 07:00:45 +0000

    using maths

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,18 +1,3 @@
 #!/usr/bin/node
-
-// import the argv module from nodejs
-const args = process.argv.slice(2);
-
-num = parseInt(args[0]);
-
-if (!isNaN(num)) {
-    console.log('My number: <first argument converted in integer>');
-}
-
-else {
-    console.log("Not a number");
-}
-// If the argument can’t be converted to an integer, print “Not a number”
-// You must use console.log(...) to print all output
-// You are not allowed to use var
-// You are not allowed to use try/catch
+const num = Math.floor(Number(process.argv[2]));
+console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
