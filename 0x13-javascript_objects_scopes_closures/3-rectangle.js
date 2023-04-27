7bd78cca87f6a9f6a50020549ea1b55f8f96ae4f
Hope_Davids <hopedavids@gmail.com>
2023-04-27 06:31:49 +0000

    adding methods in class

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,38 +1,10 @@
 #!/usr/bin/node
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+  }
 
-// Write a class Rectangle that defines a rectangle:
-
-// You must use the class notation for defining your class
-// The constructor must take 2 arguments: w and h
-// Initialize the instance attribute width with the value of w
-// Initialize the instance attribute height with the value of h
-// If w or h is equal to 0 or not a positive integer, create an empty object
-// Create an instance method called print() that prints the rectangle using the character X
-
-
-class Rectangle {
-
-    constructor(w, h) {
-        if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
-// If w or h is equal to 0 or not a positive integer, create an empty object]
-            return;
-        }
-        else {
-            this.w = w;
-            this.h = h;
-        }
-        
-    }
-
-    print() {
-       if (!this.w || !this.h) {
-        return;
-       }
-       
-       const row = 'X'.repeat(this.w);
-       for (let i = 0; i < this.h; i++) {
-            console.log(row);
-       }
-    }
-
-}
+  print () {
+    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
+  }
+};
