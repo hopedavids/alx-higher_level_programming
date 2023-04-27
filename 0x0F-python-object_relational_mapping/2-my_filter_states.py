ed77300ed4160b0aba5fd0575f2413a3879752cc
Hope_Davids <hopedavids@gmail.com>
2023-04-27 07:25:43 +0000

    filter states by name

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,33 +1,17 @@
 #!/usr/bin/python3
-""" This script lists all states from
-    the database hbtn_0e_0_usa with name
-    starting with N
-"""
-import MySQLdb as mydb
+# Displays all values in the states table of the database hbtn_0e_0_usa
+# whose name matches that supplied as argument.
+# Usage: ./2-my_filter_states.py <mysql username> \
+#                                <mysql password> \
+#                                <database name> \
+#                                <state name searched>
 import sys
+import MySQLdb
 
-def filter_states():
-    # initialize the connection to the database
-    db = mydb.connect(host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    #create the object cursor
-    cursor = db.cursor()
-    state = sys.argv[4]
-    #command goes here
-    command = ("SELECT * FROM states WHERE name is %s ORDER BY id ASC", (state))
-    #execute the command
-    cursor.execute(command)
-    #fetch all rows
-    rows = cursor.fetchall()
-    #print rows
-    if len(rows) == 0:
-        print("no state found", (state))
-    else:
-        for row in rows:
-            print(row)
-        
-    #close the cursor connection
-    cursor.close()
-    db.close()
-
-    if __name__ == "__main__":
-        filter_states()
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * \
+                 FROM `states` \
+                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
+    [print(state) for state in c.fetchall()]
