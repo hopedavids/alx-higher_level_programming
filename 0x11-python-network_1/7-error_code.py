ddaca2ba400c34c62082709944efece4722a361e
Hope_Davids <hopedavids@gmail.com>
2023-05-01 13:41:33 +0000

    print out the error code using requests

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+A Python script that takes in a URL and an email address, sends a POST request to the passed URL with the email as a parameter, and finally displays the body of the response.
+"""
+import requests as r
+import sys
+
+
+if __name__ == "__main__":
+    """ 
+    A Python script that takes in a URL and an email address, sends a POST request to the     passed URL with the email as a parameter, and finally displays the body of the respo     nse.
+    """
+    get_url = sys.argv[1]
+    response = r.get(url)
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
+    else:
+        print(response.text)
+    print(response.text)
